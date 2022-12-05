6006ca273f94cce63160ec1b5969efbd604273c8
Corey Long <96073497+CoreySLong@users.noreply.github.com>
2022-12-05 16:00:04 -0500

    Completed DML file

--- Phase2/Project-dml.sql
+++ Phase2/Project-dml.sql
@@ -4,54 +4,65 @@ SELECT
     team_wins, 
     team_losses, 
     team_ties,
-    MAX(team_win_percent) 
+    MAX(team_win_percent) AS Win_Percentage
 FROM
     team
 GROUP BY
-    teams_name limit 1; 
+    teams_name 
+ORDER BY
+    Win_Percentage DESC LIMIT 1; 
 
 
--- 2. Which player leads their team in goals?
+-- 2. What are the leading goal scorers' amounts for each team?
+SELECT
+    MAX(goals) AS Top_Amount,
+    team_name
+FROM
+    player JOIN player_stats ON (ID = player_ID)
+GROUP BY
+    team_name
+ORDER BY 
+    MAX(goals) DESC;
 
 
 -- 3. List all of the teams in alphabetical order
-select
+SELECT
     teams_name
-from
+FROM
     team
-order by
+ORDER BY
     teams_name;
 
 
 -- 4. Which player has the most goals in ocean dwellers league?
-WITH sub_table1 as (
+WITH sub_table1 AS (
     SELECT
         *
     FROM   
-        player left join player_stats on (ID = player_ID)
+        player LEFT JOIN player_stats ON (ID = player_ID)
 ), 
 sub_table2 as (
     SELECT
         * 
     FROM 
-        team as A left join league as B on (A.teams_name = B.league_team_name)
+        team AS A LEFT JOIN league AS B ON (A.teams_name = B.league_team_name)
 ),
-sub_table3 as (
+sub_table3 AS (
     SELECT  
         league_name,
         teams_name,
-        first_name as 1st_name,
-        last_name as 2nd_name,
-        MAX(goals) as Top_Goals
+        first_name AS 1st_name,
+        last_name AS 2nd_name,
+        MAX(goals) AS Top_Goals
     FROM
-        sub_table1 left join sub_table2 on (sub_table1.team_name = sub_table2.teams_name)
+        sub_table1 LEFT JOIN sub_table2 ON (sub_table1.team_name = sub_table2.teams_name)
     WHERE
         league_name = "Ocean Dwellers"
-    GROUP by
+    GROUP BY
         teams_name, league_name, first_name, last_name
 )
 SELECT
-    MAX(sub_table3.Top_Goals) as uses, 
+    MAX(sub_table3.Top_Goals) AS Top_Goals, 
     sub_table3.1st_name,
     sub_table3.2nd_name,
     league_name
@@ -61,65 +72,75 @@ GROUP BY
     sub_table3.1st_name,
     sub_table3.2nd_name
 ORDER BY
-    uses desc limit 1
+    Top_Goals DESC LIMIT 1;
 
 
 -- 5. What teams played on 10/29/2022? 
-select
+SELECT
     games_team_name,
-    opponent
-from
+    opponent,
+    games_date
+FROM
     games
 WHERE   
-    games_date = "2022-10-29"
+    games_date = "2022-10-29";
 
 
--- 6. Which teams will have 5 games remaining after mm/dd/yyyy?
+-- 6. Which teams played after 10/13/2022?
+SELECT 
+    games_team_name,
+    opponent, 
+    games_date
+FROM
+    games
+WHERE
+    games_date > '2022-10-13';
 
 
--- 7. How many times did Team A play Team B this year?
-with sub_table4 as (
-    select  
+-- 7. How many times did the Sharks play the Jags this year?
+WITH sub_table4 AS (
+    SELECT  
         games_team_name, 
         opponent, 
         games_date
-    from
+    FROM
         games
-    where
+    WHERE
         games_team_name = "Sharks" AND opponent = "Jags" OR games_team_name = "Jags" AND opponent = "Sharks"
 )
-select
-    count(games_date) as Times_Played
-from 
-    sub_table4
+SELECT
+    COUNT(games_date) AS Times_Played
+FROM 
+    sub_table4;
 
 
 -- 8.	What player has the jersey number 13?
-select 
+SELECT 
     first_name, 
     last_name
-from 
+FROM
     player
-where 
-    jersey_number = "13"
+WHERE 
+    jersey_number = "13";
 
 
 -- 9.	What teams are not in the same league as team Straw Hats?
 SELECT
     league_team_name
-from 
+FROM
     league
-where
-    league_name != "Ocean Dwellers"
+WHERE
+    league_name != "Ocean Dwellers";
 
 
 -- 10.	What teams are under .5 win rate?
 SELECT
-    teams_name
-from 
+    teams_name,
+    team_win_percent
+FROM 
     team
-where
-    team_win_percent < 50
+WHERE
+    team_win_percent < 50;
 
 
 -- 11.	What teams have the same exact record?
@@ -220,8 +241,94 @@ FROM
     JOIN sub_table8 ON (maxs = number_of)
 
 
---16.	Has this Player A played in any other league?
---17.	What teams played the most during the week of mm/dd/yyyy?
---18.	What two teams played in the last game of the season for a specific league?
---19.	What player assisted the first goal of the season in a specific league?
---20.	What league had the most saves in a specific year?
+-- 16.	What team is "Luffy D Monkey" on?
+SELECT
+    team_name
+FROM
+    player
+WHERE
+    first_name = "Luffy" AND last_name = "D Monkey"
+
+
+--17.	What teams played during the week of 10/02/2022?
+SELECT
+    games_team_name,
+    opponent,
+    games_date
+FROM
+    games
+WHERE 
+    games_date = '2022-10-2' OR games_date = '2022-10-3' OR games_date = '2022-10-4' OR  
+    games_date = '2022-10-5' OR games_date = '2022-10-6' OR games_date = '2022-10-7' OR
+    games_date = '2022-10-8'
+ORDER BY
+    games_date; 
+
+
+-- 18.	What two teams played in the most recent game?
+WITH sub_table11 as(
+    SELECT
+        max(games_date) as thedate
+    FROM
+        games
+)
+SELECT
+    games_team_name, 
+    opponent,
+    thedate
+FROM
+    games JOIN sub_table11 on (games_date = thedate)
+
+
+-- 19.	What team has played the most home games?
+SELECT
+    count(games_team_name) as Home_Games,
+    games_team_name
+FROM
+    games
+GROUP BY 
+    games_team_name
+ORDER BY 
+    Home_Games DESC LIMIT 1;
+
+
+
+--20.	What league had the most saves?
+WITH sub_table10 as ( 
+    SELECT
+        max(saves) as saves,
+        team_name
+    FROM
+        player JOIN player_stats ON (ID = player_ID)
+    GROUP BY
+        team_name
+    ORDER BY 
+        max(saves) DESC LIMIT 1
+)
+SELECT
+    saves,
+    league_name
+FROM
+    sub_table10 JOIN league ON (team_name = league_team_name);
+
+
+-- 1. What team has the best record in their league?
+-- 2. What are the leading goal scorers' amounts for each team?
+-- 3. List all of the teams in alphabetical order
+-- 4. Which player has the most goals in ocean dwellers league?
+-- 5. What teams played on 10/29/2022? 
+-- 6. Which teams played after 10/13/2022?
+-- 7. How many times did the Sharks play the Jags this year?
+-- 8.	What player has the jersey number 13?
+-- 9.	What teams are not in the same league as team Straw Hats?
+-- 10.	What teams are under .5 win rate?
+-- 11.	What teams have the same exact record?
+-- 12.	What team has the most RW (position) players?
+-- 13.	How many games had more than 5 total goals scored?
+-- 14.	Do player A and Player B play the same position?
+-- 15.	What league has the largest number of teams?
+-- 16.	What team is "Luffy D Monkey" on?
+-- 17.	What teams played during the week of 10/02/2022?
+-- 18.	What two teams played in the most recent game?
+-- 19.	What team has played the most home games?
+-- 20.	What league had the most saves?
